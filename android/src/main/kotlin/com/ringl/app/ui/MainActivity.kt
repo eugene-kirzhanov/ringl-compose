9376db4db155ecbcde47723650cb07763f1e7850
Eugene K <anegine@gmail.com>
2021-08-22 18:14:12 +0300

    Add splash screen for android app

--- android/src/main/kotlin/com/ringl/app/ui/MainActivity.kt
+++ android/src/main/kotlin/com/ringl/app/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ringl.common.compose.App
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MaterialTheme {
                 App()
